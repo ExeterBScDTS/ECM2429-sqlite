1b60a723a1b9ade88f8dd091f4a35c6e058aff85
Michael Saunby <mike@saunby.net>
2020-12-18 14:21:02 +0000

    .

--- lesson-01/code/main.py
+++ lesson-01/code/main.py
@@ -6,25 +6,25 @@ import sqlite3
 # Create a temporary database in memory.
 conn = sqlite3.connect(':memory:')
 # More usual is to have a database file, e.g.
-# conn = sqlite3.connect('stocks.db')
+# conn = sqlite3.connect('example.db')
 
 c = conn.cursor()
 
 # Create table
-c.execute('''create table stocks
-(date text, trans text, symbol text,
- qty real, price real)''')
+c.execute('''CREATE TABLE stocks
+(date text, trans text, symbol text, qty real, price real)''')
 
 # Insert a row of data
-c.execute("""insert into stocks
-          values ('2006-01-05','BUY','RHAT',100,35.14)""")
+c.execute("""INSERT INTO stocks
+          VALUES ('2006-01-05','BUY','RHAT',100,35.14)""")
 
-# Larger example
-for t in [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
-          ('2006-04-05', 'BUY', 'MSOFT', 1000, 72.00),
-          ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
-         ]:
-    c.execute('insert into stocks values (?,?,?,?,?)', t)
+
+# Larger example that inserts many records at a time
+purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
+             ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
+             ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
+            ]
+c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
 
 # Save (commit) the changes
 conn.commit()
@@ -34,7 +34,7 @@ c.close()
 
 # Now retreive the data
 c = conn.cursor()
-c.execute('select * from stocks order by price')
+c.execute('SELECT * FROM stocks ORDER by price')
 for row in c:
     print(row)
 
